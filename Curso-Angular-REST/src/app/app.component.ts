d0761abd3dcddc5ffe7b98cbc944eeb63fc455cb
luangf <luangf222@gmail.com>
2023-04-14 11:54:11 -0300

    NgModel e Formulário

--- Curso-Angular-REST/src/app/app.component.ts
+++ Curso-Angular-REST/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Curso-Angular-REST';
+  
+  //variavel, como se fosse um array/json/obj, atributo login... inicia ele zerado, toda vez q instancia a classe inicia ele instancia com o valor zerado
+  usuario={login: '', senha: ''};
+  
+  public login(){
+    console.log("Login: "+this.usuario.login+" Senha: "+this.usuario.senha);
+  }
+  
 }
